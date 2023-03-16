0c6374f083311cb2a17005054b3854f12b5df0a0
sanjanamalkar <sanjanamalkar@gmail.com>
2023-03-16 10:18:30 +0530

    Added: code of review

--- src/com/bridgelabz/CountDistinctElement.java
+++ src/com/bridgelabz/CountDistinctElement.java
@@ -6,23 +6,6 @@ import java.util.Set;
 public class CountDistinctElement {
     public static void countDistinct(int[] arr) {
         int n = arr.length;
-
-        for (int i = 0; i < n; i++) {
-            boolean isDistinct = true;
-
-            for (int j = 0; j < i; j++) {
-                if (arr[i] == arr[j]) {
-                    isDistinct = false;
-                    break;
-                }
-            }
-
-            if (isDistinct) {
-                System.out.print(arr[i] + " ");
-
-            }
-        }
-
         int distinctCount = 0;
         for (int i = 0; i < n; i++) {
             boolean isDistinct = true;
@@ -35,8 +18,17 @@ public class CountDistinctElement {
             }
 
             if (isDistinct) {
+                System.out.print(arr[i] + " ");
                 distinctCount++;
+
             }
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            boolean isDistinct = true;
+
+
         }
         System.out.println();
             System.out.println("count of distinct element is : " + distinctCount);
